9cf3ffa86b32a41b65fb6d0989ddf40c55b536f9
Esse Woods <oakninja@gmail.com>
2020-05-24 22:43:58 +0200

    Updates

---
+++ precache-manifest.08d96c6eb75b498a2d59bbf61b479b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ab2553302d1218f9a32dfe8fa05048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2517f4da21355232b476",
+    "url": "/static/css/main.9bca530d.chunk.css"
+  },
+  {
+    "revision": "fe2224dbe511bd38ad10",
+    "url": "/static/js/2.34d337f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.34d337f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2517f4da21355232b476",
+    "url": "/static/js/main.0c1f0e7a.chunk.js"
+  },
+  {
+    "revision": "3d7077356017ca96f537",
+    "url": "/static/js/runtime-main.ad2cead7.js"
+  }
+]);
